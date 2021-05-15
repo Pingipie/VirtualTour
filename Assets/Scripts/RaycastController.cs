89fd733c0153e2fa24d7a0f494c676c86c3dc453
Pietro <pingi.pie95@gmail.com>
2021-05-15 17:40:45 +0200

    Raycast non funzionante

--- Assets/Scripts/RaycastController.cs
+++ Assets/Scripts/RaycastController.cs
@@ -10,15 +10,22 @@ public class RaycastController : MonoBehaviour
 
     public bool isHitting;
 
+    public void Start()
+    {
+        line = this.GetComponent<LineRenderer>();
+    }
+
     // Update is called once per frame
     void FixedUpdate()
     {
+        line.SetPosition(0, this.transform.position);
+
         if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity))
         {
             if (hit.collider.GetComponent<InteractableObject>())
             {
                 isHitting = true;
-                hit.collider.GetComponent<InteractableObject>().Zoom();               
+                hit.collider.GetComponent<InteractableObject>();               
             }
             else
             {
